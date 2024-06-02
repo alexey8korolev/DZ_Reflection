15014a4217b9e438f3a0ba6ba887b5695d685b4d
alexey8korolev <alexeykorolev75@gmail.com>
2024-06-03 01:01:41 +0300

    ДЗ №13-14

---
+++ DZ_Reflection/Program.cs
@@ -0,0 +1,67 @@
+﻿using Reflection.Logic;
+using Reflection.Sample;
+using Reflection.Utils;
+
+
+Console.WriteLine("Нажмите любую клавишу для запуска  REFLECTION...");
+Console.ReadLine();
+
+var f_obj = new F() { i1 = 1, i2 = 2, i3 = 3, i4 = 4, i5 = 5 };
+
+Console.WriteLine("[1.Запуск] CSV сериализации");
+
+string csvString = string.Empty;
+
+Stopwatch.Start();
+for (var i = 0; i < 1000; i++)
+{
+    csvString = CsvConvert.SerializeObject<F>(f_obj);
+}
+Stopwatch.Stop();
+
+var deltaTime = Stopwatch.TimeInterval;
+
+Stopwatch.Start();
+Console.WriteLine($"[3.Закончена] CSV сериализации 1000 раз. Время сериализации : {deltaTime}");
+Console.WriteLine($"[4.CSV] полученная строка : \n{csvString}");
+Stopwatch.Stop();
+Console.WriteLine($"[6.] Время вывода текста в консоль : {Stopwatch.TimeInterval}");
+
+Stopwatch.Start();
+var obj = CsvConvert.DeserializeObject<F>(csvString);
+Stopwatch.Stop();
+
+Console.WriteLine($"[9.Десериализация] CSV в экземпляр класса: {obj}. Время десериализации : {Stopwatch.TimeInterval}");
+
+
+// JSON сериализации
+Console.WriteLine("------------  JSON --------");
+
+Console.WriteLine("[7.Запуск] JSON сериализации");
+
+string jsonString = string.Empty;
+
+Stopwatch.Start();
+for (var i = 0; i < 1000; i++)
+{
+   jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(f_obj);
+}
+Stopwatch.Stop();
+
+deltaTime = Stopwatch.TimeInterval;
+
+Stopwatch.Start();
+Console.WriteLine($"[7.] JSON сериализации 1000раз. Время сериализации : {deltaTime}");
+Console.WriteLine($"[7.JSON] Строка : \n{jsonString}");
+Stopwatch.Stop();
+Console.WriteLine($"[7.Окончание] Время вывода текста в консоль  : {Stopwatch.TimeInterval}");
+
+Stopwatch.Start();
+obj = Newtonsoft.Json.JsonConvert.DeserializeObject<F>(jsonString);
+Stopwatch.Stop();
+
+Console.WriteLine($"[7.Десериализация] JSON в экземпляр класса: {obj}. Время десериализации : {Stopwatch.TimeInterval}");
+
+Console.WriteLine("Успешное Окончание!!!");
+Console.ReadKey();
+
---
+++ DZ_Reflection/Utils/Stopwatch.cs
@@ -0,0 +1,29 @@
+﻿namespace Reflection.Utils;
+
+
+public static class Stopwatch
+{
+    public static TimeSpan TimeInterval { get; private set; }
+    private static DateTime startTime;
+    private static DateTime stopTime;
+
+    public static void Start()
+    {
+        if (startTime > stopTime)
+            throw new Exception("Необходимо запустить метод Stop");
+
+        startTime = DateTime.Now;
+    }
+
+    public static void Stop()
+    {
+        stopTime = DateTime.Now;
+
+        TimeInterval = stopTime - startTime;
+
+        if (TimeInterval < TimeSpan.Zero)
+        {
+            throw new ArgumentException("Необходимо запустить метод Start");
+        }
+    }
+}
